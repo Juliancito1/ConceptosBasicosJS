6aa5304ae5c1f9a31334afff98259a4c86b5ce4f
Julian <julianser@hotmail.com.ar>
2023-02-27 11:37:19 -0300

    terminamos ejercicio de los dias del mes

--- js/2-condicionalesEj2.js
+++ js/2-condicionalesEj2.js
@@ -3,15 +3,34 @@
 // 30 dias- abril 4, junio 6, septiembre 9, noviembre 11
 // 28 o 29 dias- febrero
 
-let mes = parseInt(prompt("Ingrese el numero del mes (1 al 12)"));
+let mes = parseInt(
+  prompt("Ingrese el numero del mes (1 al 12) 1-enero, 2-marzo")
+);
+console.log(isNaN(mes));
 
-if(mes === 1 || mes === 3 || mes === 5 || mes === 7 || mes === 8 || mes === 10 || mes === 12)
-{
-    document.write("El mes seleccionado ("+ mes +") tiene 31 dias<br>");
-    document.write(`El mes seleccionado (${mes}) tiene 31 dias`);
-} else if (mes === 2)
-{
-    document.write(`El mes seleccionado (${mes}) tiene 28 o 29 dias dependiendo del año`)
-}else{
-    document.write(`El mes seleccionado (${mes}) tiene 30 dias`);
+if (!isNaN(mes)) {
+  if (mes > 0 && mes <= 12) {
+    if (
+      mes === 1 ||
+      mes === 3 ||
+      mes === 5 ||
+      mes === 7 ||
+      mes === 8 ||
+      mes === 10 ||
+      mes === 12
+    ) {
+      document.write("El mes seleccionado (" + mes + ") tiene 31 dias<br>");
+      document.write(`El mes seleccionado (${mes}) tiene 31 dias`);
+    } else if (mes === 2) {
+      document.write(
+        `El mes seleccionado (${mes}) tiene 28 o 29 dias dependiendo del año`
+      );
+    } else {
+      document.write(`El mes seleccionado (${mes}) tiene 30 dias`);
+    }
+  } else {
+    document.write(`Ingreso un numero (${mes}) invalido`);
+  }
+} else {
+  document.write(`El valor ingresado (${mes}), no es un numero`);
 }
