afe77b1e33402bcc4cfa922bcc34d86a751bd53b
Julian <julianser@hotmail.com.ar>
2023-03-01 10:51:44 -0300

    agregamos la estructura while

---
+++ js/4-bucles.js
@@ -0,0 +1,17 @@
+// while(condicion logica){
+//      todo el codigo 
+//      cambiar la condicion logica
+// }
+
+let i = 2;
+while(i<=10)
+{
+    document.write(`<p>Este es el parrafo N:${i}</p>`)
+    i+=2 // i= i+2;
+    //i++ es igual a i= i+1
+}
+
+// do while
+
+
+// for
